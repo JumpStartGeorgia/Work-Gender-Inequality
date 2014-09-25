78e0275e5074e262fc991b81cf5d9f0862b4067e
jason <jason.addie@gmail.com>
2014-09-25 18:23:34 +0400

    got basic filter selection working with table only

--- website/app/controllers/root_controller.rb
+++ website/app/controllers/root_controller.rb
@@ -1,7 +1,16 @@
 class RootController < ApplicationController
 
   def index
-    @about = Page.find_by_name('about')
+    @questions = SurveyQuestion.can_crosstab
+
+    if request.post?
+      # check to make sure row and col param is in list of questions
+      if @questions.index{|x| x.code == params[:row]}.present? && @questions.index{|x| x.code == params[:col]}.present?
+        @data = SurveyResult.crosstab_count(params[:row], params[:col])
+      else
+        #### DO SOMETHING?
+      end
+    end
     
     respond_to do |format|
       format.html # index.html.erb
