503cc5aa405cef0dcb3f2583c7b149197bcaedbd
antarya <antarya@gmail.com>
2014-09-26 18:19:24 +0400

    Some mutation animation tries

--- gap/assets/js/main.js
+++ gap/assets/js/main.js
@@ -1,5 +1,80 @@
 $(document).ready(function(){
+  var circles = $('.tester ellipse');
+  var cir_count = circles.length;
+  var cir_medium = cir_count%2 == 0 ? [cir_count/2,cir_count/2+1] : [Math.ceil10(cir_count/2)];
+  var cir_offset = 50;
+  var cir_dur = 100;
+  console.log(cir_count,cir_medium);
 
+$('.repeat').on('click',circle_redraw);
+circle_redraw();
+  function circle_redraw()
+  {
+
+    var vl = [50,150,250];
+    circles.each(function(i,d){ $(d).attr('cx',vl[i]).attr('rx',40).show(); });
+
+    circles.each(function(i,d){    
+      var t = $(d);
+      t.attr('data-cx',t.attr('cx'));
+
+      if(i+1 < cir_medium) 
+      {
+        //console.log(i,"left");
+        t.animate({'color':'white'},{duration:cir_dur,
+          progress:function(a,b,c){
+            $(this).attr('cx',+$(this).attr('data-cx')-cir_offset*b);
+          },
+          complete:function()
+          {
+            $(this).attr('data-cx',t.attr('cx')).animate({'color':'white'},{duration:500,
+              progress:function(a,b,c){
+                $(this).attr('cx',+$(this).attr('data-cx')+130*b);
+              }
+            });          
+          }
+        });
+      }
+      else if(i+1 > cir_medium)
+      {
+        //console.log(i,"right");
+        t.animate({'color':'white'},{duration:cir_dur,
+          progress:function(a,b,c){
+            $(this).attr('cx',+$(this).attr('data-cx')+cir_offset*b);
+          },
+          complete:function()
+          {
+            $(this).attr('data-cx',t.attr('cx')).animate({'color':'white'},{duration:500,
+              progress:function(a,b,c){
+                $(this).attr('cx',+$(this).attr('data-cx')-130*b);
+               
+              },
+              complete:function()
+              {
+                 ellipse();
+              }
+            });   
+          }
+        });
+      }
+    });
+}
+function ellipse()
+{
+  $('.tester ellipse[data-id=1]').hide();
+  $('.tester ellipse[data-id=3]').hide();
+
+   $('.tester ellipse[data-id=2]').animate({'color':'white'},{duration:150,
+          progress:function(a,b,c){
+            $(this).attr('rx',20*b+40).attr("fill","url(#myLinearGradient1)");
+          },
+          complete:function()
+          {
+            $(this).attr('rx',40);
+          }    
+        });
+
+}
 // bind events
   $(document).on('DOMMouseScroll mousewheel', function(e, delta) {
 
