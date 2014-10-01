f8292e7b6885babc41abe2d4f8d544a834cead2d
jason <jason.addie@gmail.com>
2014-10-01 16:14:57 +0400

    added migration to tell to load sproc

--- db/crosstab_count_sproc.sql
+++ db/crosstab_count_sproc.sql
@@ -1,33 +1,39 @@
- drop procedure if exists survey_crosstab_count;
- 
- DELIMITER //
- CREATE PROCEDURE survey_crosstab_count(field_rows varchar(50), field_cols varchar(50))
-   BEGIN
+drop function if exists SQL_ESC;
 
-      SET @sql := CONCAT("SELECT 
-					    CONCAT('SELECT ",
-				    SQL_ESC(field_rows),
-				    ",	', GROUP_CONCAT(DISTINCT CONCAT('sum(IF(",
-                       SQL_ESC(field_cols),
-				    "=', QUOTE(",
-				    SQL_ESC(field_cols),
-				    "),', 1,0)) AS ',SQL_ESC(",
-				    SQL_ESC(field_cols),
-				    ") )), ' FROM  survey_results where ", SQL_ESC(field_rows), " != \"\" GROUP BY ",
-				    SQL_ESC(field_rows),
-				    "')
-         INTO   @sql
-         FROM   survey_results
-	      where ", SQL_ESC(field_cols), " != '' " 
-      );
+CREATE FUNCTION SQL_ESC(_identifier VARCHAR(64))
+RETURNS VARCHAR(130) DETERMINISTIC
+RETURN CONCAT('`',REPLACE(_identifier,'`','``'),'`');
 
-    PREPARE stmt FROM @sql; 
-    EXECUTE stmt; 
-    DEALLOCATE PREPARE stmt;
+drop procedure if exists survey_crosstab_count;
 
-    PREPARE stmt FROM @sql; 
-    EXECUTE stmt; 
-    DEALLOCATE PREPARE stmt;
-        
-   END //
- DELIMITER ;
+DELIMITER //
+CREATE PROCEDURE survey_crosstab_count(field_rows varchar(50), field_cols varchar(50))
+ BEGIN
+
+    SET @sql := CONCAT("SELECT 
+				    CONCAT('SELECT ",
+			    SQL_ESC(field_rows),
+			    ",	', GROUP_CONCAT(DISTINCT CONCAT('sum(IF(",
+                     SQL_ESC(field_cols),
+			    "=', QUOTE(",
+			    SQL_ESC(field_cols),
+			    "),', 1,0)) AS ',SQL_ESC(",
+			    SQL_ESC(field_cols),
+			    ") )), ' FROM  survey_results where ", SQL_ESC(field_rows), " != \"\" GROUP BY ",
+			    SQL_ESC(field_rows),
+			    "')
+       INTO   @sql
+       FROM   survey_results
+      where ", SQL_ESC(field_cols), " != '' " 
+    );
+
+  PREPARE stmt FROM @sql; 
+  EXECUTE stmt; 
+  DEALLOCATE PREPARE stmt;
+
+  PREPARE stmt FROM @sql; 
+  EXECUTE stmt; 
+  DEALLOCATE PREPARE stmt;
+      
+ END //
+DELIMITER ;
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140925101024) do
+ActiveRecord::Schema.define(:version => 20141001120250) do
 
   create_table "page_translations", :force => true do |t|
     t.integer  "page_id"
