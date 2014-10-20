8380e143849f555b1bebbcbafea382b86a14c405
antarya <antarya@gmail.com>
2014-10-20 18:06:55 +0400

    Gap game moved to public folder, working with routes

--- app/controllers/gap_controller.rb
+++ app/controllers/gap_controller.rb
@@ -1,14 +1,10 @@
 class GapController < ApplicationController
-  def index
-    respond_to do |format|
-      format.html # index.html.erb
-    end
-  end
+  
 
-  def poll
-    logger.debug(params);
-    respond_to do |format|
-      format.js { render :json { :id => "jiSDF322A(@@$Fda" }.to_json }
-    end
-  end
+  # def poll
+  #   logger.debug(params);
+  #   respond_to do |format|
+  #     format.json { render :json { :id => "jiSDF322A(@@$Fda" }.to_json }
+  #   end
+  # end
 end
