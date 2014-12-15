eddbe1da5608280a9d4e67ff50de2eeb6c4153d4
jason <jason.addie@gmail.com>
2014-12-15 13:12:01 +0400

    added videos; fixed headers on home page so collapse better

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -4,32 +4,57 @@ $(document).ready(function(){
     // make row > divs the same heights
     var heights = [];
     var height;
-    $('#get-involved-content > div').each(function(){
-      heights.push($(this).height());
-    });
-    height = Math.max.apply(Math, heights);
-    $('#get-involved-content > div').each(function(){
-      $(this).height(height);
-    });
-    // just set height of game to be height of row
-    $('#get-involved-content > div#game').height($('#get-involved-content').height());
 
-    heights = [];
-    $('#latest-content > div').each(function(){
-      heights.push($(this).height());
-    });
-    height = Math.max.apply(Math, heights);
-    $('#latest-content > div').each(function(){
-      $(this).height(height);
-    });
+    // if window width is too small, just use default height
+    // else make them the same
+    if ($(window).width() < 769){
+      $('#get-involved-content > div').each(function(){
+        $(this).height('auto');
+      });
 
+      $('#latest-content > div').each(function(){
+        $(this).height('auto');
+      });
+    }else{
+      // get height of stories and explore and then use the max
+      heights.push($('#get-involved-content #stories').height());
+      heights.push($('#get-involved-content #explore').height());
+      height = Math.max.apply(Math, heights);
+      $('#get-involved-content > div').each(function(){
+        $(this).height(height);
+      });
+
+      // just set height of game to be height of row
+      $('#get-involved-content > #game').height($('#get-involved-content').height());
+
+      // latest items row
+      heights = [];
+      $('#latest-content > div').each(function(){
+        heights.push($(this).height());
+      });
+      height = Math.max.apply(Math, heights);
+      $('#latest-content > div').each(function(){
+        $(this).height(height);
+      });
+    }
   }
 
+  // video width is set to 100%
+  // - when window changes size, fix height
+  //   to match 16x9 video
+  function resize_videos(){
+    var w = $('#video-content iframe:first').width();
+    $('#video-content iframe').height(w*9/16);
+  }
+
+  // have to wait until image loads to resize get involved blocks
   $(window).load(function(){
     resize_front_page();
   })
+  resize_videos();
 
   $(window).bind('resize', resize_front_page);
+  $(window).bind('resize', resize_videos);
 
 
 });
